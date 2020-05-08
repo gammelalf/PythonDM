e70caf08c36e0a47d48c831c6fac820833a87eee
gammelalf <>
2020-05-09 00:55:48 +0200

    Made Character more flexible by removing __slots__

--- character.py
+++ character.py
@@ -3,30 +3,8 @@ import os
 
 from dice import *
 
+
 class Character(object):
-
-    __slots__ = [
-                "name",
-                "max_hp",
-                "hp",
-                "ac",
-                "str",
-                "dex",
-                "con",
-                "int",
-                "wis",
-                "cha"
-            ]
-
-    __defaults__ = {
-            "ac": 10,
-            "str": 0,
-            "dex": 0,
-            "con": 0,
-            "int": 0,
-            "wis": 0,
-            "cha": 0
-            }
 
     def __init__(self, name, sheet=None, **kwargs):
         self.name = name
@@ -44,15 +22,6 @@ class Character(object):
         if isinstance(self.max_hp, str):
             self.max_hp = eval(self.max_hp)
         self.hp = self.max_hp
-
-    def __getattr__(self, key):
-        try:
-            return self.__getattribute__(key)
-        except AttributeError as e:
-            if e.args == (key,) and key in Character.__defaults__:
-                return Character.__defaults__[key]
-            else:
-                raise e
 
     @property
     def _global_name(self):
