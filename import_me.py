a2b81d3c747e115837a5f95794d88635ff3564b3
gammelalf <>
2020-05-10 15:28:54 +0200

    Made start be runable from any directory

--- import_me.py
+++ import_me.py
@@ -1,7 +1,9 @@
 from dice import *
-from battle import *
-from character import *
+from battle import Battle
+from sheet import SheetDirectory
+from util import real_path, Context
 
-enemys = CharacterFactory("enemys")
+sheets = SheetDirectory(real_path("sheets"))
+enemys = sheets.enemys
 
 Context._set_globals(globals())
--- util.py
+++ util.py
@@ -2,13 +2,21 @@ import json
 import os
 import sys
 
+def real_path(path):
+    """
+    Make a path who is relative to this project's directory
+    relative to the current working directory.
+    """
+    return os.path.join(os.path.dirname(sys.argv[0]), path)
+
+
 def json_load(path):
     """
     Load a json file.
 
-    The `path` is relative to the program's folder.
+    The `path` is relative to the project's folder.
     """
-    with open(os.path.join(os.path.dirname(sys.argv[0]), path)) as f:
+    with open(real_path(path)) as f:
         return json.load(f)
 
 
