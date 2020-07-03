19199a4b9b237fb6a94a5522afa75fac936ebc28
gammelalf <>
2020-07-03 19:03:39 +0200

    Commented import_me.py

--- import_me.py
+++ import_me.py
@@ -4,17 +4,24 @@ from sheet import SheetDirectory, Sheet
 from util import real_path, Context
 from character import Player, Character
 
+# Load sheets
 sheets = SheetDirectory(real_path("sheets"))
 enemys = sheets.enemys
 
+# Put all sheets from sheets/players/ as Characters into a list
 players = []
 for player in sheets.players.__dict__:
     players.append(Player(getattr(sheets.players, player)))
 
+# Clear namespace
+del player
+
+# Get a fresh new Battle object with the players already added
 def reset_battle():
     global battle
     battle = Battle()
     battle._add(players)
 reset_battle()
 
+# Enable Contexts
 Context._set_globals(globals())
