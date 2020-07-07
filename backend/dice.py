357c9e4e71a5a22aa39fb0e1f32d8b141e95fcf5
gammelalf <>
2020-07-07 20:42:03 +0200

    Made some tweaks for the last commit

--- backend/dice.py
+++ backend/dice.py
@@ -18,7 +18,7 @@ import re
 # ================== Dices ================== #
 def normal(n):
     """
-    Roll a normal n sided dice.
+    Roll a normal ´n´ sided dice.
     """
     return random.randint(1, n)
 
@@ -45,30 +45,30 @@ def __gaussint(lower, upper, sigma=1, radius=3):
 
 def gauss(n):
     """
-    Roll a n sided dice who follows a normal distribution.
+    Roll a ´n´ sided dice who follows a normal distribution.
     """
     return __gaussint(1, n)
 
 
 def lowest(n):
     """
-    Return ´1´.
+    Return 1
     """
     return 1
 
 
 def highest(n):
     """
-    Return ´n´.
+    Return ´n´
     """
     return n
 
 
 def expected(n):
     """
-    Return half of ´n´.
+    Return expected value of a ´n´ sided dice.
     """
-    return n//2
+    return (n+1)/2
 
 
 # ================== Evaluator ================== #
@@ -76,6 +76,10 @@ __dice_pattern = re.compile(r"([1-9]\d*)?d([1-9]\d*)")
 
 
 def roll(expr, dice=normal):
+    """
+    Evaluate all dice operations in the expression using the given ´dice´
+    and hand the rest to python's eval function.
+    """
     # find all dice operations
     matches = []
     match = __dice_pattern.search(expr)
